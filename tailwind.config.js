a2ad5476c587ad15376447baa5485324d29dd771
Monir <mrmonir0558@gmail.com>
2024-09-25 20:04:53 +0600

    project setup done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+                Inter: ["Inter", "sans-serif"],
+            },
+        },
+    },
+    plugins: [require("daisyui")],
+};
